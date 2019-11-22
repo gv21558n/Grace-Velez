cdad347447f9bcb753534b936923f2d4c73da976
gv21558n <58083198+gv21558n@users.noreply.github.com>
2019-11-22 11:21:29 -0500

    Add files via upload

---
+++ VelezGMadLibs.py
@@ -0,0 +1,13 @@
+e=input("Do you like dogs")
+print(e)
+d=input("do you like cats")
+print(d)
+c=input("what is your favorite food")
+print(c)
+b=input("How old are you")
+print(b)
+a=input("Where are you from")
+print(a)
+
+
+print("I am from " + a +", I am " + b + "My favorite food is " + c + "My dogs favorite food is " + c + "Does my cat like my dog? " + d + "Does my dog like my cat? " + e)
